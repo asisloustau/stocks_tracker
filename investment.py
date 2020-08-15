2e24ef2f17ed27d1ac4a0b27b08f38d7194fbc9a
Asis Loustau <asisloustau@gmail.com>
2020-08-14 20:29:39 -0700

    Added store investment and stock history in db

--- investment.py
+++ investment.py
@@ -1,6 +1,9 @@
 import yfinance
 from yfinance import Ticker
-import datetime
+from datetime import datetime, timedelta
+import sqlite3
+
+DB_NAME = 'investments.db'
 
 
 class Investment(Ticker):
@@ -20,12 +23,20 @@ class Investment(Ticker):
 
         self.amount = amount
 
-        if isinstance(date, datetime.datetime):
+        if isinstance(date, datetime):
             self.date = date
         else:
-            self.date = datetime.datetime.strptime(date, "%m-%d-%Y")
+            self.date = datetime.strptime(date, "%m-%d-%Y")
 
         super().__init__(self.ticker)  # inherit __init__ from Ticker
+
+        # Get stock data from yfinance for dates starting the investment date
+        self.data = self.history(start=self.date.strftime("%Y-%m-%d"))
+
+        # convert Timestamp indexes to str datetime for sqlite
+        # WARNING this will have to be changed in more robust sql engines to preserve date format
+        self.data.index = self.data.index.strftime(
+            "%Y-%m-%d") + f"_{self.ticker}"
 
     def __str__(self):
         return f"Investment({self.ticker},${self.amount},{self.date})"
@@ -33,4 +44,87 @@ class Investment(Ticker):
     def __repr__(self):
         return self.__str__()
 
-    # def get_stock_data(self):
+    def store_investment_data(self):
+
+        # sqlite connect to database
+        conn = sqlite3.connect(DB_NAME)
+        c = conn.cursor()
+        # create investments table if not exists
+        c.execute(
+            """
+                CREATE TABLE IF NOT EXISTS investments
+                (
+                    date_created text,
+                    date_invested text,
+                    ticker text,
+                    amount real
+                )
+            """
+        )
+
+        # add investment to database
+        current_date = datetime.now().strftime("%Y-%m-%d")
+        c.execute(
+            f"""
+        INSERT INTO investments 
+        VALUES (
+            '{current_date}',
+            '{self.date.strftime("%Y-%m-%d")}',
+            '{self.ticker}',
+            '{self.amount}'
+            )
+            """
+        )
+
+        # commit and close
+        conn.commit()
+        conn.close()
+
+        return True
+
+    def get_stock_data(self):
+        """
+        Return DataFrame with stock data for investment instance
+        """
+        return self.data
+
+    def store_stock_data(self):
+        """
+        Populate database with stock historical data
+        """
+
+        # sqlite connect to database
+        conn = sqlite3.connect(DB_NAME)
+        c = conn.cursor()
+        # create investments table if not exists
+        c.execute(
+            """
+                CREATE TABLE IF NOT EXISTS stock_history
+                (
+                    date text primary key,
+                    open real,
+                    high real,
+                    low real,
+                    close real,
+                    volume int,
+                    dividends int,
+                    'stock splits' int
+                )
+            """
+        )
+        # store DataFrame in db table - careful, if existing dates in db, it will throw an error
+        # TODO: either (1) add each df row independently and check if primary exists or (2) when filling new data make sure to check last value
+        self.data.to_sql('stock_history', con=conn, if_exists='append')
+        return True
+
+
+# TODO:
+# Fetch data from database and return a dataframe with, for each ticker in investments:
+# figure out regex to join tables -date_ticker
+# ticker | initial_value | current_value | profit_loss | profit_loss_pct (for now, can add more views/features)
+
+# Any time user checks data:
+# check current date
+# check last available date in database for each ticker
+# get data for all tickers found in database (yfinance.Tickers)
+# store data in database (NOTE we could do some sort of class inheritance/mixin to use store_stock_data from investment)
